d761bbc8a2d2d691e27a6d1eec1af39497b9769e
박원창 <parkwc0213@naver.com>
2023-04-22 22:29:34 +0900

    code signal 1~10 solved

--- leetcode/Q22_Generate_Parentheses.java
+++ leetcode/Q22_Generate_Parentheses.java
@@ -1,5 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Q22_Generate_Parentheses {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 
     }
 }
